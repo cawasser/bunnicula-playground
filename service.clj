5e2fff4d0411dd556fd2bdd3be4c19cbde0f0d28
Chris Wasser <cawasser@mac.com>
2020-01-07 00:00:21 -0500

    added more testing: adding users, and performing simple calculations (adding 2 numbers, pub on 1 queue, get result on a 2nd queue)

--- service.clj
+++ service.clj
@@ -15,6 +15,10 @@
                                     :password "guest"
                                     :vhost "/main"}))
 
+(def some-queue "some.queue")
+(def user-mgmt-queue "user-mgmt")
+(def calculate-queue "calculate")
+(def calculate-result-queue "calculate-result")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -33,6 +37,8 @@
                                       [:rmq-connection])
                          :rmq-connection connection)
                      component/start-system))
+(def pub (partial protocol/publish (:publisher server-system)))
+
 
 
 ; this works!
@@ -47,10 +53,20 @@
                {:integration_id 4 :message_id "4th"}
                {:integration_id 5 :message_id "fifth-symphony"}])
 
-(def pub (partial protocol/publish (:publisher server-system) "some.queue"))
+(def user-mgmt-messages [{:user "chris" :password "kdfigukds9548dj" :roles [:admin :repo :assign]}])
+
+(def calculate-messages [{:x 100 :y 500}
+                         {:x 7 :y 3}])
+
 
 (doall
-  (map (partial protocol/publish (:publisher server-system) "some.queue") messages))
+  (map (partial pub "some.queue") messages))
+
+(doall
+  (map (partial pub user-mgmt-queue) user-mgmt-messages))
+
+(doall
+  (map (partial pub calculate-queue) calculate-messages))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -62,6 +78,8 @@
 ; stores the messages we've seen so far
 
 (def message-received (atom []))
+(def users (atom []))
+(def calculations (atom []))
 
 (defn import-conversation-handler
   [body parsed envelope components]
@@ -83,26 +101,97 @@
 
     :ack))
 
+(defn user-mgmt-handler
+  [body parsed envelope components]
+  (let [{:keys [user password roles]} parsed]
 
-(def consumer (consumer/create {:message-handler-fn import-conversation-handler
-                                :options {:queue-name "some.queue"
-                                          :exchange-name "my-exchange"
-                                          :timeout-seconds 120
-                                          :backoff-interval-seconds 60
-                                          :consumer-threads 4
-                                          :max-retries 3}}))
+    (swap! users conj [user password roles])
+
+    :ack))
+
+
+(defn calculation-handler
+  [body parsed envelope components]
+  (let [{:keys [x y]} parsed]
+
+    (pub calculate-result-queue {:x x :y y :result (+ x y)})
+
+    :ack))
+
+
+(defn calculation-result-handler
+  [body parsed envelope components]
+  (let [{:keys [x y result]} parsed]
+
+    (swap! calculations conj {:x x :y y :result result})
+
+    :ack))
+
+
+(def defaults {:options {:exchange-name "my-exchange"
+                         :timeout-seconds 120
+                         :backoff-interval-seconds 60
+                         :consumer-threads 4
+                         :max-retries 3}})
+
+(defn setup-queue [handler queue-name]
+  (-> defaults
+    (assoc :message-handler-fn handler)
+    (assoc-in [:options :queue-name] queue-name)))
+
+
+(def message-consumer (consumer/create {:message-handler-fn import-conversation-handler
+                                        :options {:queue-name "some.queue"
+                                                  :exchange-name "my-exchange"
+                                                  :timeout-seconds 120
+                                                  :backoff-interval-seconds 60
+                                                  :consumer-threads 4
+                                                  :max-retries 3}}))
 
 ;
 ; this "system" just runs until shutdown, processing messages from "some.queue"
 ;
-(def client-system (-> (component/system-map
-                         :rmq-connection connection
-                         :monitoring monitoring/BaseMonitoring
-                         :consumer (component/using
-                                     consumer
-                                     [:rmq-connection :monitoring]))
-                     component/start-system))
+(def message-system (-> (component/system-map
+                          :rmq-connection connection
+                          :monitoring monitoring/BaseMonitoring
+                          :consumer (component/using
+                                      message-consumer
+                                      [:rmq-connection :monitoring]))
+                      component/start-system))
 
+
+(def user-system
+  (-> (component/system-map
+        :rmq-connection connection
+        :monitoring monitoring/BaseMonitoring
+        :consumer (component/using
+                    (consumer/create
+                      (setup-queue user-mgmt-handler
+                        user-mgmt-queue))
+                    [:rmq-connection :monitoring]))
+    component/start-system))
+
+
+(def calc-system
+  (-> (component/system-map
+        :rmq-connection connection
+        :monitoring monitoring/BaseMonitoring
+        :consumer (component/using
+                    (consumer/create (setup-queue calculation-handler
+                                       calculate-queue))
+                    [:rmq-connection :monitoring]))
+    component/start-system))
+
+
+(def calc-result-system
+  (-> (component/system-map
+        :rmq-connection connection
+        :monitoring monitoring/BaseMonitoring
+        :consumer (component/using
+                    (consumer/create (setup-queue calculation-result-handler
+                                       calculate-result-queue))
+                    [:rmq-connection :monitoring]))
+    component/start-system))
 
 
 
@@ -110,9 +199,34 @@
 ;
 ; let's send some more messages
 
-(pub {:integration_id 234 :message_id "we need this"})
-message-received ; it shows!
+(pub "some.queue" {:integration_id 234 :message_id "we need this"})
+message-received
+; it shows!
 
 
-(pub {:integration_id 6783 :message_id "and again"})
-message-received ; it shows!
+(pub some-queue {:integration_id 6783 :message_id "and again"})
+message-received
+; it shows!
+
+
+(pub user-mgmt-queue {:user "steve" :password "dkktiu-4" :roles [:view-only]})
+users
+
+
+(pub calculate-result-queue {:x 100 :y 200 :result (+ 100 200)})
+calculations
+
+
+(pub calculate-queue {:x 100 :y 200})
+calculations
+
+
+; this test publishing to 1 queue, and looking for the results on a 2nd queue
+;
+(pub calculate-queue {:x 82 :y 20})
+(pub calculate-queue {:x 9 :y 17})
+(pub calculate-queue {:x 3 :y 42})
+calculations
+
+
+
