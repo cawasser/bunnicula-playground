a5f484fce585780ec4ab196bf5cebad8d73a6e7f
Chris Wasser <cawasser@mac.com>
2020-01-19 01:56:28 -0500

    have enough protobuf working to create an item, publish it to a queue, and consume it off the queue. Next: reconstitute the original value in the consumer. Includes the person.proto schema definition, as well as the compile java implementation classes (under target/java/). requires manually compiling the proto and java code

--- protobuf_queue.clj
+++ protobuf_queue.clj
@@ -5,29 +5,30 @@
             [bunnicula.protocol :as protocol]
             [bunnicula.component.monitoring :as monitoring]
             [bunnicula.component.consumer-with-retry :as consumer]
-            [protobuf.core :as pb]))
+            [protobuf.core :as protobuf]))
 
 
-; compile each *proto into *.java
 ;
-; protoc -I=resources/proto --java_out=target/java resources/proto/message.proto
+; compile the *.proto(s) into *.java:
 ;
-;
-;
+; protoc -I=/usr/include -I=/usr/local/include -I=resources/proto --java_out=src/java resources/proto/*.proto
 ;
 ;
-; now compile the *.java into *.class (not correct yet)
 ;
-; javac -d target/classes target/java/com/bunnicula_playground/Message.java
+;
+; then compile the *.java into *.class (not correct yet):
+;
+; javac -I=/usr/local/include -d target/classes src/java/com/example/tutorial/Example.java
 
 
 
-(import '(com.bunnicula_playground Message$Person))
+(import 'com.example.tutorial.Example$Person)
+(import 'java.io.ByteArrayInputStream)
 
 
 
 
-(def alice (protobuf/create Message$Person
+(def alice (protobuf/create Example$Person
              {:id 108
               :name "Alice"
               :email "alice@example.com"}))
@@ -38,9 +39,16 @@
          (assoc :likes ["climbing" "running" "jumping"])
          (protobuf/->bytes)))
 
-
+alice
+b
 
 (protobuf/bytes-> alice b)
+
+; some additional fooling around
+;
+
+(protobuf/schema Example$Person)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -96,14 +104,19 @@
                                       [:rmq-connection])
                          :rmq-connection connection)
                      component/start-system))
+
 (def pub (partial protocol/publish (:publisher server-system)))
 
 
 
-; this works!
-(protocol/publish (:publisher server-system)
-  "some.queue"
-  {:integration_id 1 :message_id "123"}) ; this works!
+; put some kind of pb content, like "alice" on the queue
+;
+(pub "some.queue" alice)
+  ; this one looks "un-mangled" so we just get back an edn map
+
+(pub "some.queue" b)
+  ; this one looks "protobuf'd" so the :parsed data is a binary jumble(?)
+
 
 
 
@@ -115,26 +128,49 @@
 
 
 ; stores the messages we've seen so far
-
+;
 (def message-received (atom []))
 
-(defn -handler
+
+(defn pb-handler
   [body parsed envelope components]
-  (let [{:keys [integration_id message_id]} parsed]
 
-    ;; ... do whatever processing is needed ...
-    ;;
-    ;; then return one of:
-    ;;
-    ;;     :ack   - it worked!
-    ;;     :error - it didn't work and it wont work in the future (syntax error in the request, etc.)
-    ;;     :retry - it didn't work, but retrying it might work the next time (3rd service unavailable, etc).
-    ;;
-    ;;
-    ;; NOTE: this is a "QUEUE" so we don't send the "reply" down this same path, we send the reply some other
-    ;;       way, like another queue
+  (swap! message-received conj {:body body
+                                :parsed parsed})
+  :ack)
 
-    (swap! message-received conj parsed)
 
-    :ack))
+; hook our handler into the mechanism (including error and retry)
+;
+(def message-consumer (consumer/create {:message-handler-fn pb-handler
+                                        :options {:queue-name "some.queue"
+                                                  :exchange-name "my-exchange"
+                                                  :timeout-seconds 120
+                                                  :backoff-interval-seconds 60
+                                                  :consumer-threads 4
+                                                  :max-retries 3}}))
+
+
+; this "system" just runs until shutdown, processing messages from "some.queue"
+;
+(def message-system (-> (component/system-map
+                          :rmq-connection connection
+                          :monitoring monitoring/BaseMonitoring
+                          :consumer (component/using
+                                      message-consumer
+                                      [:rmq-connection :monitoring]))
+                      component/start-system))
+
+
+
+
+(:parsed (get @message-received 0))
+
+
+; we've received the "b" messages off the queue, but it's "binary", let's
+; see if we can recover the edn
+;
+(def parsed (:parsed (get @message-received 1)))
+(def body (:body (get @message-received 1)))
+(protobuf/read alice (ByteArrayInputStream. body))
 
